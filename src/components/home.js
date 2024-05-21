957ba9387051f18b39e7545daf24f3785f6db71c
ishimwepati <wazacode@gmail.com>
2024-05-21 10:30:02 +0300

    H1 Removed

--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AnotherPage = () => {
   return (
     <div className="another-page">
-      <h1>Weoclem</h1>
+    
       <p>You can add any content here for your another page.</p>
       <p>New Page</p>
       <div>
